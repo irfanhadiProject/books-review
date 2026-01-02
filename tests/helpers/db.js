88abad0b58d78e6f4df4ce90edfee6e8a6161325
Irfan <irfanhadi619@gmail.com>
2026-01-02 13:39:27 +0700

    test(db): make resetDB deterministic and safe for test env

--- tests/helpers/db.js
+++ tests/helpers/db.js
@@ -5,12 +5,7 @@ export async function resetDb() {
     throw new Error('resetDb is only allowed in test environment')
   }
 
-  await db.query('BEGIN')
-  try {
-    await db.query('TRUNCATE user_books, books, users RESTART IDENTITY CASCADE')
-    await db.query('COMMIT')
-  } catch (err) {
-    await db.query('ROLLBACK')
-    throw err
-  }
+  await db.query(
+    'TRUNCATE user_books, books, users RESTART IDENTITY CASCADE'
+  )
 }
