daec21f60293f676d720d2df0c1200690bcde69a
Irfan <irfanhadi619@gmail.com>
2026-01-02 13:46:28 +0700

    fix(auth): handle UserNotFoundError as 401 response

--- src/utils/mapDomainErrorToHttpError.js
+++ src/utils/mapDomainErrorToHttpError.js
@@ -1,10 +1,12 @@
 import { ValidationError as DomainValidationError } from '../domain/errors/ValidationError.js'
 import { UserAlreadyHasBookError } from '../domain/errors/UserAlreadyHasBookError.js'
 import { DatabaseError as DomainDatabaseError } from '../domain/errors/DatabaseError.js'
+import { UserNotFoundError } from '../domain/errors/UserNotFoundError.js'
 
 import { ValidationError } from '../http/errors/ValidationError.js'
 import { ConflictError } from '../http/errors/ConflictError.js'
 import { DatabaseError } from '../http/errors/DatabaseError.js'
+import { AuthError } from '../http/errors/AuthError.js'
 
 export function mapDomainErrorToHttpError(err) {
   if (err instanceof DomainValidationError) {
@@ -13,6 +15,10 @@ export function mapDomainErrorToHttpError(err) {
 
   if (err instanceof UserAlreadyHasBookError) {
     return new ConflictError(err.message)
+  }
+
+  if (err instanceof UserNotFoundError) {
+    return new AuthError('Invalid username or password')
   }
 
   if (err instanceof DomainDatabaseError) {
