b666e9a1c955d5b3d73f1fbb20ddc125d4b863b1
Irfan <irfanhadi619@gmail.com>
2026-01-06 16:46:52 +0700

    fix(domain-error): add .js extension to ESM import

--- src/domain/errors/InvalidPasswordError.js
+++ src/domain/errors/InvalidPasswordError.js
@@ -1,4 +1,4 @@
-import { DomainError } from './DomainError'
+import { DomainError } from './DomainError.js'
 
 export class InvalidPasswordError extends DomainError {
   constructor(message = 'Invalid password') {
